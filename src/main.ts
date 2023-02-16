26e00664cdb869b8793004a623b1226210574abe
David Garcia <Davidgarciama@outlook.com>
2023-02-16 12:05:11 -0600

    Solution

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,16 @@
-// Given a list of integers and a single sum value, return the first two values (parse from the left please) in order of appearance that add up to form the sum.
-
-// If there are two or more pairs with the required sum, the pair whose second element has the smallest index is the solution.
-
-
-// sum_pairs([11, 3, 7, 5],         10)
-// #              ^--^      3 + 7 = 10
-// == [3, 7]
-
-// sum_pairs([4, 3, 2, 3, 4],         6)
-// #          ^-----^         4 + 2 = 6, indices: 0, 2 *
-// #             ^-----^      3 + 3 = 6, indices: 1, 3
-// #                ^-----^   2 + 4 = 6, indices: 2, 4
-// #  * the correct answer is the pair whose second value has the smallest index
-// == [4, 2]
-
-// sum_pairs([0, 0, -2, 3], 2)
-// #  there are no pairs of values that can be added to produce 2.
-// == None/nil/undefined (Based on the language)
-
-// sum_pairs([10, 5, 2, 3, 7, 5],         10)
-// #              ^-----------^   5 + 5 = 10, indices: 1, 5
-// #                    ^--^      3 + 7 = 10, indices: 3, 4 *
-// #  * the correct answer is the pair whose second value has the smallest index
-// == [3, 7]
-
-// Negative numbers and duplicate numbers can and will appear.
-
-// NOTE: There will also be lists tested of lengths upwards of 10,000,000 elements. Be sure your code doesn't time out.
-
-export function sumPairs(ints: number[], s: number): [number, number] | void {  
-    return undefined // your code here...
-  }
-
+let sumPairs=(ints: number[], s: number): [number, number] | void =>{
+    const obj: { [key: number]: number } = {};
+    for (let i:number = 0; i < ints.length; i++) {
+        const complement:number = s - ints[i];
+        if (complement in obj) {
+            return [ints[obj[complement]], ints[i]];
+        }
+            obj[ints[i]] = i;
+    }
+    return undefined;
+}
 //Testing
-// console.log(sumPairs([11,3,7,5]),10)
-// console.log(sumPairs([4,3,2,3,4]),6)
-// console.log(sumPairs([0,0,-2,3]),2)
-// console.log(sumPairs([10,5,2,3,7,5]),10)
+// console.log(sumPairs([11,3,7,5],10)) //[3,7]
+// console.log(sumPairs([4,3,2,3,4],6)) //[4,2]
+// console.log(sumPairs([0,0,-2,3],2)) //undefined
+// console.log(sumPairs([10,5,2,3,7,5],10)) //[3,7]
